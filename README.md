776b7033956d5e0f0ef4721dc739767bc867a8df
DESKTOP-U2GNR6R\15914 <1591491800@qq.com>
2023-06-07 11:49:27 +0800

    二分查找

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Leetcode-note
 leetcode刷题记录
+
+# 数组
+## 二分查找
+- 704.二分查找 easy
---
+++ 数组/二分查找/704.二分查找.js
@@ -0,0 +1,14 @@
+var search = function(nums, target) {
+    let left = 0, right = nums.length;
+    while(left < right){
+        let mid = left + ((right - left ) >> 1);
+        if(nums[mid] > target){
+            right = mid;
+        } else if(nums[mid] < target){
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+};
