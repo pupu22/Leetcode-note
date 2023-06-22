ca87edf96a95534b20c977161b7752dcd4ad0b7c
pupu22 <1591491800@qq.com>
2023-06-22 22:33:31 +0800

    二叉树的迭代遍历法

--- 二叉树/二叉树的中序遍历.js
+++ 二叉树/二叉树的中序遍历.js
@@ -3,15 +3,34 @@ function TreeNode(val,left, right){
     this.left = (left === null? null : left)
     this.right = (right === null? null : right)
 }
-var inorderTravelsal = function(node){
+// 递归法
+// var inorderTravelsal = function(node){
+//     let res = []
+//     const dfs = function(root){
+//         if(root === null)return; 
+//         dfs(root.left)
+//         res.push(root.val)
+//         dfs(root.right)
+//     }
+//     dfs(node)
+//     return res
+// }
+
+// 迭代法
+var inorderTravelsal = function (node){
     let res = []
-    const dfs = function(root){
-        if(root === null)return; 
-        dfs(root.left)
-        res.push(root.val)
-        dfs(root.right)
+    let stack = []
+    let cur = node
+    while(stack.length || cur){
+        if(cur){
+            stack.push(cur)
+            cur = cur.left
+        } else {
+            cur = stack.pop()
+            res.push(cur.val)
+            cur = cur.right
+        }
     }
-    dfs(node)
     return res
 }
 let a = new TreeNode(2,new TreeNode(3,null, null), new TreeNode(4,null, null))
--- 二叉树/二叉树的后序遍历.js
+++ 二叉树/二叉树的后序遍历.js
@@ -3,16 +3,31 @@ function TreeNode(val,left, right){
     this.left = (left === null? null : left)
     this.right = (right === null? null : right)
 }
-var postorderTravelsal = function(node){
+// 递归法
+// var postorderTravelsal = function(node){
+//     let res = []
+//     const dfs = function(root){
+//         if(root === null)return; 
+//         dfs(root.left)
+//         dfs(root.right)
+//         res.push(root.val)
+//     }
+//     dfs(node)
+//     return res
+// }
+
+// 迭代法
+var postorderTravelsal = function (node) {
     let res = []
-    const dfs = function(root){
-        if(root === null)return; 
-        dfs(root.left)
-        dfs(root.right)
-        res.push(root.val)
+    const stack = [node]
+    let cur = null
+    while(stack.length){
+        cur = stack.pop()
+        res.push(cur.val)
+        cur.left && stack.push(cur.left)
+        cur.right && stack.push(cur.right)
     }
-    dfs(node)
-    return res
+    return res.reverse()
 }
 let a = new TreeNode(2,new TreeNode(3,null, null), new TreeNode(4,null, null))
 let tree = new TreeNode(1,null, a)
